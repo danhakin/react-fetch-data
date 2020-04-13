a0b451390c1f146bea34ef56401c331ca7d157c3
danhakin <mdvillalobos@gmail.com>
2020-04-13 19:54:50 +0200

    Refactor actions with constants

--- src/hooks/fetchData.js
+++ src/hooks/fetchData.js
@@ -1,13 +1,19 @@
 import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const ACTIONS = {
+  init: "FETCH_INIT",
+  success: "FETCH_SUCCESS",
+  failure: "FETCH_FAILURE"
+};
+
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
-    case "FETCH_INIT":
+    case ACTIONS.init:
       return { ...state, isLoading: true, isError: false };
-    case "FETCH_SUCCESS":
+    case ACTIONS.success:
       return { ...state, isLoading: false, data: action.payload };
-    case "FETCH_FAILURE":
+    case ACTIONS.failure:
       return { ...state, isLoading: false, isError: true };
     default:
       throw new Error();
@@ -25,13 +31,13 @@ export const useDataApi = (initialUrl, initialData = {}) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: "FETCH_INIT" });
+      dispatch({ type: ACTIONS.init });
 
       try {
         const result = await axios(url);
-        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        dispatch({ type: ACTIONS.success, payload: result.data });
       } catch (error) {
-        dispatch({ type: "FETCH_FAILURE" });
+        dispatch({ type: ACTIONS.failure });
       }
     };
     fetchData();

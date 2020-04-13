d1a1a14cbd7520af07a7da24fc69b15406abf384
danhakin <mdvillalobos@gmail.com>
2020-04-13 14:14:58 +0200

    Define query search topic in input

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 
-const initialState = {
-  hits: []
-};
-
 function App() {
-  const [data, setData] = useState(initialState);
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=redux"
+        `https://hn.algolia.com/api/v1/search?query=${query}`
       );
       setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
-    <ul>
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
       <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
       </ul>
-    </ul>
+    </Fragment>
   );
 }
 

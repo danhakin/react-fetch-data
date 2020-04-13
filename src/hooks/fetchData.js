07cd22404b86fcd955790d15af013959e8cd71d2
danhakin <mdvillalobos@gmail.com>
2020-04-13 19:49:06 +0200

    Refactor useDataApi hooks with useReducer

--- src/hooks/fetchData.js
+++ src/hooks/fetchData.js
@@ -1,28 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_SUCCESS":
+      return { ...state, isLoading: false, data: action.payload };
+    case "FETCH_FAILURE":
+      return { ...state, isLoading: false, isError: true };
+    default:
+      throw new Error();
+  }
+};
+
 export const useDataApi = (initialUrl, initialData = {}) => {
-  const [data, setData] = useState(initialData);
   const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+      dispatch({ type: "FETCH_INIT" });
 
       try {
         const result = await axios(url);
-        setData(result.data);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (error) {
-        setIsError(error);
+        dispatch({ type: "FETCH_FAILURE" });
       }
-
-      setIsLoading(false);
     };
     fetchData();
   }, [url]);
 
-  return [{ data, isLoading, isError }, setUrl];
+  return [state, setUrl];
 };
